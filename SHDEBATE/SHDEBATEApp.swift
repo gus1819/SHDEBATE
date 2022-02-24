943d08571a43ca4e4f2dde1dfb3222aa5ee15783
이승현 <2sh2589@kakao.com>
2022-02-24 18:27:27 +0900

    Initial Commit

---
+++ SHDEBATE/SHDEBATEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHDEBATEApp.swift
+//  SHDEBATE
+//
+//  Created by 이승현 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SHDEBATEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
